abfabe54131087c8535877a090d8d60a2d62974f
Ishan Sharma <f2016773@pilani.bits-pilani.ac.in>
2020-09-26 19:09:34 +0530

    26/09

--- DSA/GeeksForGeeks/DSA-Self-Paced/Graph/BFS of graph.cpp
+++ DSA/GeeksForGeeks/DSA-Self-Paced/Graph/BFS of graph.cpp
@@ -3,26 +3,29 @@
 
 using namespace std;
 
-vector <int> bfs(vector<int> g[], int N);
+vector<int> bfs(vector<int> g[], int N);
 
-int main() {
+int main()
+{
     int T;
     cin >> T;
-    while (T--) {
+    while (T--)
+    {
         int N, E;
         cin >> N >> E;
         vector<int> adj[N];
-        for (int i = 0; i < E; i++) {
+        for (int i = 0; i < E; i++)
+        {
             int u, v;
             cin >> u >> v;
             adj[u].push_back(v);
         }
-        vector <int> res = bfs(adj, N);
-        for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
+        vector<int> res = bfs(adj, N);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
         cout << endl;
     }
-}// } Driver Code Ends
-
+} // } Driver Code Ends
 
 /* You have to complete this function*/
 
@@ -30,31 +33,31 @@ int main() {
 *  g[]: adj list of the graph
 *  N : number of vertices
 */
-vector<int> bfs(vector<int> g[], int N) 
+vector<int> bfs(vector<int> g[], int N)
 {
     int s = 0;
     vector<bool> visited(N, false);
 
     vector<int> ans;
-    
+
     queue<int> q;
     q.push(s);
     visited[s] = true;
-    
-    while(!q.empty())
+
+    while (!q.empty())
     {
         int node = q.front();
         q.pop();
-        
+
         ans.push_back(node);
-        
-        for(auto i:g[node])
-            if(!visited[i])
+
+        for (auto i : g[node])
+            if (!visited[i])
             {
                 visited[i] = true;
                 q.push(i);
             }
     }
-    
+
     return ans;
 }
--- DSA/GeeksForGeeks/DSA-Self-Paced/Graph/DFS of Graph.cpp
+++ DSA/GeeksForGeeks/DSA-Self-Paced/Graph/DFS of Graph.cpp
@@ -1,10 +1,8 @@
 // { Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
-
+// } Driver Code Ends
 
 /* Function to do DFS of graph
 
@@ -16,60 +14,60 @@ return a list containing the DFS traversal of the given graph
 void dfsrec(int s, vector<int> g[], vector<bool> vis, vector<int> &ans)
 {
     vis[s] = true;
-    
+
     ans.push_back(s);
-    
+
     vector<int>::iterator i;
-    
-    for(i = g[s].begin(); i != g[s].end(); ++i)
+
+    for (i = g[s].begin(); i != g[s].end(); ++i)
     {
-        if(!vis[*i])
+        if (!vis[*i])
             dfsrec(*i, g, vis, ans);
     }
 }
 
 vector<int> dfs(vector<int> g[], int N)
 {
-    
-    // Iterative - 
-    
+
+    // Iterative -
+
     int s = 0;
     vector<bool> vis(N, false);
     vector<int> ans;
-    
+
     stack<int> st;
     st.push(s);
-    
+
     ans.push_back(s);
     vis[s] = true;
-    
-    while(!st.empty())
+
+    while (!st.empty())
     {
         int top = st.top();
         int flag = 0;
-        
-        for(int i=0; i<g[top].size(); i++)
+
+        for (int i = 0; i < g[top].size(); i++)
         {
-            if(!vis[g[top][i]])
+            if (!vis[g[top][i]])
             {
                 st.push(g[top][i]);
-                
+
                 ans.push_back(g[top][i]);
-                
+
                 vis[g[top][i]] = true;
-                
+
                 flag = 1;
-                
+
                 break;
             }
         }
-        
-        if(!flag) 
+
+        if (!flag)
             st.pop();
     }
-    
+
     return ans;
-    
+
     /* Recursive
     
     vector<bool> vis(N, false);
@@ -85,30 +83,29 @@ vector<int> dfs(vector<int> g[], int N)
 int main()
 {
     int T;
-    cin>>T;
-    while(T--)
+    cin >> T;
+    while (T--)
     {
 
         int N, E;
-        cin>>N>>E;
-        
+        cin >> N >> E;
+
         vector<int> g[N];
         bool vis[N];
-        
+
         memset(vis, false, sizeof(vis));
-        
-        for(int i=0;i<E;i++)
+
+        for (int i = 0; i < E; i++)
         {
-            int u,v;
-            cin>>u>>v;
+            int u, v;
+            cin >> u >> v;
             g[u].push_back(v);
             g[v].push_back(u);
         }
 
-        vector <int> res = dfs(g, N);
-        for (int i = 0; i < res.size (); i++)
+        vector<int> res = dfs(g, N);
+        for (int i = 0; i < res.size(); i++)
             cout << res[i] << " ";
-        cout<<endl;
-
+        cout << endl;
     }
-}  // } Driver Code Ends
+} // } Driver Code Ends
