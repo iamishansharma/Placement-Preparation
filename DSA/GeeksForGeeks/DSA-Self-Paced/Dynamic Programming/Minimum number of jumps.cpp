5631f4a51c21c6b27d3c3cd318ef168f8f45c46e
Ishan Sharma <sharmaishan747@gmail.com>
2020-11-19 10:03:33 +0530

    MInimum Jumps Better

--- DSA/GeeksForGeeks/DSA-Self-Paced/Dynamic Programming/Minimum number of jumps.cpp
+++ DSA/GeeksForGeeks/DSA-Self-Paced/Dynamic Programming/Minimum number of jumps.cpp
@@ -1,5 +1,5 @@
 // { Driver Code Starts
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -9,45 +9,38 @@ int main()
 {
     //taking count of testcases
     int t;
-    cin >> t;
-    while (t--)
+    cin>>t;
+    while(t--)
     {
-
-        int n, i, j;
-        cin >> n;
+        
+        int n,i,j;
+        cin>>n;
         int arr[n];
-        for (int i = 0; i < n; i++)
-            cin >> arr[i];
-
-        cout << minimumJumps(arr, n) << endl; // Function to calculate minimum number of jumps
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+            
+        cout<<minimumJumps(arr, n)<<endl;    // Function to calculate minimum number of jumps
     }
     return 0;
-} // } Driver Code Ends
+}// } Driver Code Ends
+
 
 // function to find minimum number of jumps to reach end of the array
 int minimumJumps(int arr[], int n)
 {
-    int jumps[n];
-
-    for (int i = 1; i < n; i++)
-        jumps[i] = INT_MAX;
-
-    jumps[0] = 0;
-
-    for (int i = 0; i < n; i++)
+    int dp[n];
+    
+    dp[0] = 0;
+    
+    for(int i=1; i<n; i++)
     {
-        if (jumps[i] == INT_MAX)
-            return -1;
-
-        int temp = arr[i];
-        int j = i + 1;
-
-        while (temp--)
-        {
-            if (j < n)
-                jumps[j] = min(jumps[j], 1 + jumps[i]);
-            j++;
-        }
+        dp[i] = INT_MAX;
+        
+        for(int j=0; j<i; j++)
+            if(arr[j] + j >= i)
+                if(dp[j] != INT_MAX)
+                    dp[i] = min(dp[i], dp[j] + 1);
     }
-    return jumps[n - 1];
+    
+    return dp[n-1];
 }
