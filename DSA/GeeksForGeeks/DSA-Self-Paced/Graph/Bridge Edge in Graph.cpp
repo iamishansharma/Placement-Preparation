485b3f262e6b7fceef5404fda5cb1d30d6e1b09c
Ishan Sharma <sharmaishan747@gmail.com>
2020-12-23 17:39:24 +0530

    Tarjan's Algorithm

--- DSA/GeeksForGeeks/DSA-Self-Paced/Graph/Bridge Edge in Graph.cpp
+++ DSA/GeeksForGeeks/DSA-Self-Paced/Graph/Bridge Edge in Graph.cpp
@@ -40,26 +40,42 @@ adj : adjacency list of the graph
 V : Number of vertices
 s,e : End vertices of the given edge
 */
-void DFS(list<int> adj[], vector<bool> &visited, int start)
-{
-    visited[start] = true;
 
-    for (auto it = adj[start].begin(); it != adj[start].end(); it++)
-        if (!visited[*it])
-            DFS(adj, visited, *it);
+void DFS(int u, list<int> adj[], vector<int> &disc, vector<int> &parent, vector<int> &low, int &t)
+{
+    disc[u] = low[u] = t++;
+
+    for (auto it = adj[u].begin(); it != adj[u].end(); it++)
+    {
+        int v = *it;
+
+        if (disc[v] == -1)
+        {
+            parent[v] = u;
+
+            DFS(v, adj, disc, parent, low, t);
+
+            low[u] = min(low[u], low[v]);
+
+            // if(low[v] > disc[u])
+            //     cout<<"This is the one: "<<u<<" "<<v<<endl;
+        }
+        else if (v != parent[u])
+            low[u] = min(low[u], disc[v]);
+    }
 }
 
 bool isBridge(list<int> adj[], int V, int s, int e)
 {
-    adj[s].remove(e);
-    adj[e].remove(s);
+    vector<int> disc(V, -1);
+    vector<int> parent(V, -1);
+    vector<int> low(V, -1);
 
-    vector<bool> visited(V, false);
+    int t = 1;
 
-    DFS(adj, visited, s);
-
-    if (!visited[e])
-        return true;
-    else
-        return false;
+    for (int i = 0; i < V; i++)
+        if (disc[i] == -1)
+            DFS(i, adj, disc, parent, low, t);
+    //cout<<endl;
+    return low[e] > disc[s] || low[s] > disc[e];
 }
--- DSA/GeeksForGeeks/DSA-Self-Paced/Graph/Strongly connected component (Tarjans's Algo).cpp
+++ DSA/GeeksForGeeks/DSA-Self-Paced/Graph/Strongly connected component (Tarjans's Algo).cpp
@@ -32,65 +32,51 @@ int main()
 } // } Driver Code Ends
 
 // adj : adjacency list of the given graph
-// N : number of vertices
-void SCCUtil(int u, vector<int> adj[], vector<int> &disc, vector<int> &low, stack<int> &st, vector<bool> &stackMember)
+// V : number of vertices
+
+void DFS(int u, vector<int> adj[], vector<bool> &recSt, stack<int> &st, vector<int> &disc, vector<int> &low, int &t)
 {
-    static int time = 0;
-
-    // Initialize discovery time and low value
-    disc[u] = low[u] = ++time;
+    disc[u] = low[u] = t++;
     st.push(u);
-    stackMember[u] = true;
+    recSt[u] = true;
 
-    // Go through all vertices adjacent to this
-    for (auto i = adj[u].begin(); i != adj[u].end(); ++i)
+    for (int i = 0; i < adj[u].size(); i++)
     {
-        int v = *i; // v is current adjacent of 'u'
+        int v = adj[u][i];
 
-        // If v is not visited yet, then recur for it
         if (disc[v] == -1)
         {
-            SCCUtil(v, adj, disc, low, st, stackMember);
-
-            // Check if the subtree rooted with 'v' has a
-            // connection to one of the ancestors of 'u'
-            // Case 1 (per above discussion on Disc and Low value)
+            DFS(v, adj, recSt, st, disc, low, t);
             low[u] = min(low[u], low[v]);
         }
-
-        // Update low value of 'u' only of 'v' is still in stack
-        // (i.e. it's a back edge, not cross edge).
-        // Case 2 (per above discussion on Disc and Low value)
-        else if (stackMember[v] == true)
+        else if (recSt[v] == true)
             low[u] = min(low[u], disc[v]);
     }
 
-    // head node found, pop the stack and print an SCC
-    int w = 0; // To store stack extracted vertices
     if (low[u] == disc[u])
     {
         while (st.top() != u)
         {
-            w = st.top();
-            cout << w << " ";
-            stackMember[w] = false;
+            cout << st.top() << " ";
+            recSt[st.top()] = false;
             st.pop();
         }
-        w = st.top();
-        cout << w << ",";
-        stackMember[w] = false;
+        cout << st.top() << ",";
+        recSt[st.top()] = false;
         st.pop();
     }
 }
 
 void find(vector<int> adj[], int V)
 {
+    stack<int> st;
+    vector<bool> recSt(V, false);
     vector<int> disc(V, -1);
     vector<int> low(V, -1);
-    vector<bool> stackMember(V, false);
-    stack<int> st;
+
+    int t = 1;
 
     for (int i = 0; i < V; i++)
         if (disc[i] == -1)
-            SCCUtil(i, adj, disc, low, st, stackMember);
+            DFS(i, adj, recSt, st, disc, low, t);
 }
